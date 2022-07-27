6750d54314b27f301c9d471ec45bef363eb7e721
DuongDo2410 <66940364+DuongDo2410@users.noreply.github.com>
2022-07-27 09:28:09 +0700

    login & resgiter

--- router/route.js
+++ router/route.js
@@ -1,14 +1,28 @@
 const Router = require("router");
 const DashboardController = require("../controller/dashboard.controller");
 const HomeController = require("../controller/index.controller");
+const UserController = require("../controller/user.controller");
 const router = Router();
 const homeController = new HomeController();
 const dashboardController = new DashboardController();
-
+const userController = new UserController();
 // home page
-router.get("/", homeController.showHomePage);
+router.get("/", userController.readSession, homeController.showHomePage);
 
 //dashboard page
-router.get("/dashboard", dashboardController.showPage);
+router.get(
+  "/dashboard",
+  userController.readSession,
+  dashboardController.showPage
+);
+
+router.get("/register", userController.showRegisterPage);
+
+router.get("/login", userController.showLoginPage);
+router.get("/logout", userController.logout);
+
+router.post("/register", userController.regiter);
+
+router.post("/login", userController.login);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -23,10 +23,22 @@ let server = http.createServer((req, res) => {
   const filesDefences = trimPath.match(
     /\.js|\.css|\.png|\.svg|\.jpg|\.ttf|\.woff|\.woff2|\.eot/
   );
+  // if (filesDefences) {
+  //   const extension = mimeTypes[filesDefences[0].toString().split(".")[1]];
+  //   res.writeHead(200, { "Content-Type": extension });
+  //   fs.createReadStream(__dirname + req.url).pipe(res);
+  // }
   if (filesDefences) {
-    const extension = mimeTypes[filesDefences[0].toString().split(".")[1]];
+    let filePath = filesDefences[0].toString();
+    let extension = mimeTypes[filesDefences[0].toString().split(".")[1]];
+    if (filePath.includes("/css")) {
+      extension = mimeTypes[filesDefences[0].toString().split("/")[2]];
+    }
+    if (extension.includes("?")) {
+      extension = extension.split("?")[0];
+    }
     res.writeHead(200, { "Content-Type": extension });
-    fs.createReadStream(__dirname + req.url).pipe(res);
+    fs.createReadStream(__dirname + "/" + req.url).pipe(res);
   } else {
     router(req, res, finalhandler(req, res));
   }
