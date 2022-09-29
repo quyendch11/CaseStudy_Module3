1bb67969403895f072765051af6606227516dc48
dinhthang1310 <dinhthang9513@gmail.com>
2022-09-29 14:35:38 +0700

    addes && search product

---
+++ react_js/shopping/src/pages/Signup.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+Signup.propTypes = {
+
+};
+
+function Signup(props) {
+    return (
+        <div>Signup</div>
+    );
+}
+
+export default Signup;
